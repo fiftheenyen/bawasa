3b61113c131ed93e0188bb17043d36d163da418d
Rhovic Ataiza <devrhovic@gmail.com>
2025-08-17 20:34:22 +0800

    changed the name to signin

--- consummer_app/lib/core/router.dart
+++ consummer_app/lib/core/router.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../views/auth/welcome_screen.dart';
-import '../views/auth/login_screen.dart';
+import '../views/auth/signin_screen.dart';
 import '../views/dashboard/dashboard_screen.dart';
 
 final consumerRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, _) => const WelcomeScreen()),
-    GoRoute(path: '/login', builder: (context, _) => const LoginScreen()),
+    GoRoute(path: '/login', builder: (context, _) => const SigninScreen()),
     GoRoute(
       path: '/dashboard',
       builder: (context, _) => const DashboardScreen(),
