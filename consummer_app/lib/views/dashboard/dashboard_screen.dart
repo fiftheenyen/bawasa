3b4629d382f836f7afda7b90dc8401a1c222e2e1
Rhovic Ataiza <devrhovic@gmail.com>
2025-08-18 15:14:44 +0800

    changed stateless to stateful

--- consummer_app/lib/views/dashboard/dashboard_screen.dart
+++ consummer_app/lib/views/dashboard/dashboard_screen.dart
@@ -12,9 +12,14 @@ class SizingConfig {
   }
 }
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
